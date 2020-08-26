b22b1d71a53faec35a65f35d56eb62fb3f7e769b
Elijah Yap <yap@usc.edu>
2020-08-25 17:54:49 -0700

    Update grovepi_sensors.py

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
         OBJ_PRES = ""
         if n_range < threshhold:
             OBJ_PRES = "OBJ PRES"
-            setText_norefresh(128, 0, 0)
+            setRGB(128, 0, 0)
         else:
-            setText_norefresh(0, 128, 0)
-        setText(f"{threshhold:3}cm {OBJ_PRES}\n{n_range:3}cm")
+            setRGB(0, 128, 0)
+        setText_norefresh(f"{threshhold:3}cm {OBJ_PRES}\n{n_range:3}cm")
